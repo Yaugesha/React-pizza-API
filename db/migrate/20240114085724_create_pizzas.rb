5d7b8931c9281a37165c210cc774d2229ac2732e
Yaugesha <eugene.korchemenko@gmail.com>
2024-01-14 13:40:19 +0300

    init project

---
+++ db/migrate/20240114085724_create_pizzas.rb
@@ -0,0 +1,12 @@
+class CreatePizzas < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.float :unitPrice
+      t.string :imageUrl
+      t.boolean :soldOut
+
+      t.timestamps
+    end
+  end
+end
