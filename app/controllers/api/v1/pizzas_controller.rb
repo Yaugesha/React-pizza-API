d6d56181f74b33248277189e7f7e9834557cc12d
Yaugesha <eugene.korchemenko@gmail.com>
2024-03-04 22:57:39 +0300

    feat: implement updating pizza

--- app/controllers/api/v1/pizzas_controller.rb
+++ app/controllers/api/v1/pizzas_controller.rb
@@ -17,14 +17,9 @@ class Api::V1::PizzasController < ApplicationController
 
   # POST /pizzas
   def create
-    ingredients_params =  JSON.parse(params[:pizza][:ingredients])
-    pizza_params_without_ingredients = pizza_params.except(:ingredients)
-
-    ingredients = ingredients_params.collect { |ingredient| Ingredient.find(ingredient["id"]) }
-
     @pizza = Pizza.new(pizza_params_without_ingredients)
 
-    @pizza.ingredients << ingredients
+    add_ingredients
 
     if @pizza.save
       render json: PizzaSerializer.new(@pizza).serializable_hash[:data][:attributes], status: :created
@@ -35,7 +30,9 @@ class Api::V1::PizzasController < ApplicationController
 
   # PATCH/PUT /pizzas/1
   def update
-    if @pizza.update(pizza_params)
+    update_ingredients
+
+    if @pizza.update(pizza_params_without_ingredients)
       render json: @pizza
     else
       render json: @pizza.errors, status: :unprocessable_entity
@@ -51,10 +48,45 @@ class Api::V1::PizzasController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pizza
       @pizza = Pizza.find(params[:id])
+      @pizza_ingredients = @pizza.ingredients
     end
 
     # Only allow a list of trusted parameters through.
     def pizza_params
-      params.require(:pizza).permit(:name, :unitPrice, :soldOut, :image, ingredients: [:id, :name])
+      params.require(:pizza).permit(:name, :unitPrice, :soldOut, :image, ingredients: [:id, :name, :numberOfPizzas, :popularity])
+    end
+
+    def pizza_params_without_ingredients
+      pizza_params.except(:ingredients)
+    end
+
+    def ingredients_from_params
+      ingredients_params = JSON.parse(params[:pizza][:ingredients])
+      ingredients_params.collect { |ingredient| Ingredient.find(ingredient["id"]) }
+    end
+
+
+    def add_ingredients
+      ingredients = ingredients_from_params
+      @pizza.ingredients << ingredients
+    end
+
+    def update_ingredients
+      ingredients = ingredients_from_params
+
+      # add new ingredients
+      ingredients.each do |new_ingredient|
+        unless @pizza_ingredients.include? new_ingredient
+          @pizza_ingredients << new_ingredient
+        end
+      end
+
+      # remove deleted ingredients
+      @pizza_ingredients.each do |curent_ingredient|
+        unless ingredients.include? curent_ingredient
+          p "remove"
+          @pizza_ingredients.destroy curent_ingredient
+        end
+      end
     end
 end
--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,4 +1,15 @@
 class IngredientSerializer
   include JSONAPI::Serializer
   attributes :id, :name
+
+  attribute :numberOfPizzas do |ingredient|
+    PizzaIngredient.where(ingredient: ingredient.id).length
+  end
+
+  attribute :popularity do |ingredient|
+    ordered_pizzas = OrderItem.all.length
+    pizzas_with_ingredient = ingredient.pizzas
+    ordered_pizzas_with_ingredient = pizzas_with_ingredient.sum { |pizza| pizza.order_items.length }
+    ordered_pizzas_with_ingredient / ordered_pizzas * 100
+  end
 end
